b94071cb2152b5c1f742f0925b3ff576e2d97d6f
Luis Daniel Castillo Arriola <ldcastil@bancoagricola.com.sv>
2021-06-07 02:31:38 -0600

    Agregar archivos de proyecto.

---
+++ Guia8/CGrafo.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia8
+{
+    class CGrafo
+    {
+        public List<CVertice> nodos; // Lista de nodos del grafo
+        public CGrafo() // Constructor
+        {
+            nodos = new List<CVertice>();
+        }
+        //=====================Operaciones Básicas=================================
+        //Construye un nodo a partir de su valor y lo agrega a la lista de nodos
+        public CVertice AgregarVertice(string valor)
+        {
+            CVertice nodo = new CVertice(valor);
+            nodos.Add(nodo);
+            return nodo;
+        }
+        //Agrega un nodo a la lista de nodos del grafo
+        public void AgregarVertice(CVertice nuevonodo)
+        {
+            nodos.Add(nuevonodo);
+        }
+        //Busca un nodo en la lista de nodos del grafo
+        public CVertice BuscarVertice(string valor)
+        {
+            return nodos.Find(v => v.Valor == valor);
+        }
+        //Crea una arista a partir de los valores de los nodos de origen y de destino
+        public bool AgregarArco(string origen, string nDestino, int peso = 1)
+        {
+            CVertice vOrigen, vnDestino;
+            //Si alguno de los nodos no existe, se activa una excepción
+            if ((vOrigen = nodos.Find(v => v.Valor == origen)) == null)
+                throw new Exception("El nodo " + origen + " no existe dentro del grafo");
+            if ((vnDestino = nodos.Find(v => v.Valor == nDestino)) == null)
+                throw new Exception("El nodo " + nDestino + " no existe dentro del grafo");
+            return AgregarArco(vOrigen, vnDestino);
+        }
+        // Crea la arista a partir de los nodos de origen y de destino
+        public bool AgregarArco(CVertice origen, CVertice nDestino, int peso = 1)
+        {
+            if (origen.ListaAdyacencia.Find(v => v.nDestino == nDestino) == null)
+            {
+                origen.ListaAdyacencia.Add(new CArco(nDestino, peso));
+                return true;
+            }
+            return false;
+        }
+        // Método para dibujar el grafo
+        public void DibujarGrafo(Graphics g)
+        {
+            // Dibujando los arcos
+            foreach (CVertice nodo in nodos)
+                nodo.DibujarArco(g);
+
+            // Dibujando los vértices
+            foreach (CVertice nodo in nodos)
+                nodo.DibujarVertice(g);
+        }
+        public CVertice DetectarPunto(Point posicionMouse)
+        {
+            foreach (CVertice nodoActual in nodos)
+                if (nodoActual.DetectarPunto(posicionMouse)) return nodoActual;
+            return null;
+        }
+        // Método para regresar al estado original
+        public void ReestablecerGrafo(Graphics g)
+        {
+            foreach (CVertice nodo in nodos)
+            {
+                nodo.Color = Color.White;
+                nodo.FontColor = Color.Black;
+                foreach (CArco arco in nodo.ListaAdyacencia)
+                {
+                    arco.grosor_flecha = 1;
+                    arco.color = Color.Black;
+                }
+            }
+            DibujarGrafo(g);
+        }
+
+        public void ColoArista(string o, string d)
+        {
+            foreach (CVertice nodo in nodos)
+            {
+                foreach (CArco a in nodo.ListaAdyacencia)
+                {
+                    if (nodo.ListaAdyacencia != null && nodo.Valor == o && a.nDestino.Valor == d)
+                    {
+                        a.color = Color.Red;
+                        a.grosor_flecha = 4;
+                    }
+                }
+            }
+        }
+
+        public void Colorear(CVertice nodo)
+        {
+            nodo.Color = Color.AliceBlue;
+            nodo.FontColor = Color.Black;
+        }
+
+        public CVertice nododistanciaminima()
+        {
+            int min = int.MaxValue;
+            CVertice temp = null;
+            foreach (CVertice origen in nodos)
+            {
+                if (origen.Visitado)
+                {
+                    foreach (CVertice destino in nodos)
+                    {
+                        if (!destino.Visitado)
+                        {
+                            foreach (CArco a in origen.ListaAdyacencia)
+                            {
+                                if (a.nDestino == destino && min > a.peso)
+                                {
+                                    min = a.peso;
+                                    temp = destino;
+                                }  }  } } } }
+            return temp;
+        }
+
+        public int posicionNodo(string Nodo)
+        {
+            for (int i = 0; i < nodos.Count; i++)
+            {
+                if (String.Compare(nodos[i].Valor, Nodo) == 0)
+                    return i;
+            }
+            return -1;
+        }
+        //Funcion para re-dibujar los arcos que llegan a un nodo
+        public void DibujarEntrantes(CVertice nDestino)
+        {
+            foreach (CVertice nodo in nodos)
+            {
+                foreach (CArco a in nodo.ListaAdyacencia)
+                {
+                    if (nodo.ListaAdyacencia != null && nodo != nDestino)
+                    {
+                        if (a.nDestino == nDestino)
+                        {
+                            a.color = Color.Black;
+                            a.grosor_flecha = 2;
+                            break;
+                        }  } } }  }
+
+        //funcion que desmarca como visitados todos los nodos del grafo
+        public void Desmarcar()
+        {
+            foreach (CVertice n in nodos)
+            {
+                n.Visitado = false;
+                n.Padre = null;
+                n.distancianodo = int.MaxValue;
+                n.pesoasignado = false;
+            }
+        }
+
+
+    }
+}
---
+++ Guia8/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Guia8
+{
+    partial class Simulador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.nuevoVerticeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnElArc = new System.Windows.Forms.Button();
+            this.btnEliminarVer = new System.Windows.Forms.Button();
+            this.CBArco = new System.Windows.Forms.ComboBox();
+            this.CBVertice = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnAnch = new System.Windows.Forms.Button();
+            this.btnProf = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.CBNodoPartida = new System.Windows.Forms.ComboBox();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.btnDistancia = new System.Windows.Forms.Button();
+            this.Pizarra = new System.Windows.Forms.Panel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(288, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SIMULADOR DE GRAFOS";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuevoVerticeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 26);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // nuevoVerticeToolStripMenuItem
+            // 
+            this.nuevoVerticeToolStripMenuItem.Name = "nuevoVerticeToolStripMenuItem";
+            this.nuevoVerticeToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.nuevoVerticeToolStripMenuItem.Text = "Nuevo Vertice";
+            this.nuevoVerticeToolStripMenuItem.Click += new System.EventHandler(this.nuevoVerticeToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(409, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(174, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Click derecho para crear un Nodo ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.BtnElArc);
+            this.groupBox1.Controls.Add(this.btnEliminarVer);
+            this.groupBox1.Controls.Add(this.CBArco);
+            this.groupBox1.Controls.Add(this.CBVertice);
+            this.groupBox1.Location = new System.Drawing.Point(16, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 80);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Arco:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Vértice:";
+            // 
+            // BtnElArc
+            // 
+            this.BtnElArc.Location = new System.Drawing.Point(130, 46);
+            this.BtnElArc.Name = "BtnElArc";
+            this.BtnElArc.Size = new System.Drawing.Size(64, 23);
+            this.BtnElArc.TabIndex = 2;
+            this.BtnElArc.Text = "Eliminar";
+            this.BtnElArc.UseVisualStyleBackColor = true;
+            this.BtnElArc.Click += new System.EventHandler(this.BtnElArc_Click);
+            // 
+            // btnEliminarVer
+            // 
+            this.btnEliminarVer.Location = new System.Drawing.Point(130, 17);
+            this.btnEliminarVer.Name = "btnEliminarVer";
+            this.btnEliminarVer.Size = new System.Drawing.Size(64, 23);
+            this.btnEliminarVer.TabIndex = 0;
+            this.btnEliminarVer.Text = "Eliminar";
+            this.btnEliminarVer.UseVisualStyleBackColor = true;
+            this.btnEliminarVer.Click += new System.EventHandler(this.BtnEliminarVer_Click);
+            // 
+            // CBArco
+            // 
+            this.CBArco.FormattingEnabled = true;
+            this.CBArco.Location = new System.Drawing.Point(52, 46);
+            this.CBArco.Name = "CBArco";
+            this.CBArco.Size = new System.Drawing.Size(72, 21);
+            this.CBArco.TabIndex = 1;
+            // 
+            // CBVertice
+            // 
+            this.CBVertice.FormattingEnabled = true;
+            this.CBVertice.Location = new System.Drawing.Point(52, 19);
+            this.CBVertice.Name = "CBVertice";
+            this.CBVertice.Size = new System.Drawing.Size(72, 21);
+            this.CBVertice.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnAnch);
+            this.groupBox2.Controls.Add(this.btnProf);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.CBNodoPartida);
+            this.groupBox2.Location = new System.Drawing.Point(16, 125);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 80);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Recorridos";
+            // 
+            // btnAnch
+            // 
+            this.btnAnch.Location = new System.Drawing.Point(110, 51);
+            this.btnAnch.Name = "btnAnch";
+            this.btnAnch.Size = new System.Drawing.Size(76, 23);
+            this.btnAnch.TabIndex = 5;
+            this.btnAnch.Text = "Anchura";
+            this.btnAnch.UseVisualStyleBackColor = true;
+            this.btnAnch.Click += new System.EventHandler(this.BtnAnch_Click);
+            // 
+            // btnProf
+            // 
+            this.btnProf.Location = new System.Drawing.Point(15, 51);
+            this.btnProf.Name = "btnProf";
+            this.btnProf.Size = new System.Drawing.Size(77, 23);
+            this.btnProf.TabIndex = 3;
+            this.btnProf.Text = "Profundidad";
+            this.btnProf.UseVisualStyleBackColor = true;
+            this.btnProf.Click += new System.EventHandler(this.BtnProf_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 22);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(86, 13);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "Nodo de partida:";
+            // 
+            // CBNodoPartida
+            // 
+            this.CBNodoPartida.FormattingEnabled = true;
+            this.CBNodoPartida.Location = new System.Drawing.Point(99, 19);
+            this.CBNodoPartida.Name = "CBNodoPartida";
+            this.CBNodoPartida.Size = new System.Drawing.Size(72, 21);
+            this.CBNodoPartida.TabIndex = 4;
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Location = new System.Drawing.Point(13, 411);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(0, 13);
+            this.lblRespuesta.TabIndex = 10;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.txtBuscar);
+            this.groupBox4.Controls.Add(this.label8);
+            this.groupBox4.Controls.Add(this.BtnBuscar);
+            this.groupBox4.Location = new System.Drawing.Point(16, 211);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 65);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(47, 22);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(76, 20);
+            this.txtBuscar.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Vértice";
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(129, 20);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(64, 23);
+            this.BtnBuscar.TabIndex = 7;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            this.BtnBuscar.Click += new System.EventHandler(this.BtnBuscar_Click);
+            // 
+            // btnDistancia
+            // 
+            this.btnDistancia.Location = new System.Drawing.Point(63, 282);
+            this.btnDistancia.Name = "btnDistancia";
+            this.btnDistancia.Size = new System.Drawing.Size(92, 40);
+            this.btnDistancia.TabIndex = 6;
+            this.btnDistancia.Text = "Distancia";
+            this.btnDistancia.UseVisualStyleBackColor = true;
+            // 
+            // Pizarra
+            // 
+            this.Pizarra.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Pizarra.Location = new System.Drawing.Point(222, 39);
+            this.Pizarra.Name = "Pizarra";
+            this.Pizarra.Size = new System.Drawing.Size(527, 363);
+            this.Pizarra.TabIndex = 0;
+            this.Pizarra.Paint += new System.Windows.Forms.PaintEventHandler(this.Pizarra_Paint);
+            this.Pizarra.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Pizarra_MouseDown);
+            this.Pizarra.MouseLeave += new System.EventHandler(this.Pizarra_MouseLeave);
+            this.Pizarra.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Pizarra_MouseMove);
+            this.Pizarra.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Pizarra_MouseUp);
+            // 
+            // Simulador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(779, 431);
+            this.Controls.Add(this.Pizarra);
+            this.Controls.Add(this.btnDistancia);
+            this.Controls.Add(this.lblRespuesta);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Simulador";
+            this.Text = "Simulador Grafos";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem nuevoVerticeToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BtnElArc;
+        private System.Windows.Forms.Button btnEliminarVer;
+        private System.Windows.Forms.ComboBox CBArco;
+        private System.Windows.Forms.ComboBox CBVertice;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnAnch;
+        private System.Windows.Forms.Button btnProf;
+        private System.Windows.Forms.ComboBox CBNodoPartida;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.Button btnDistancia;
+        private System.Windows.Forms.Panel Pizarra;
+    }
+}
+
